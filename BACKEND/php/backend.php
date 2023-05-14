e87d16135fb0e019d5d2a7f33dfe21955a896bd8
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-05-14 16:15:59 -0500

    Se suben cambios

--- BACKEND/php/backend.php
+++ BACKEND/php/backend.php
@@ -1,6 +1,21 @@
 <?php
-if ($_GET) {
-    $nombre = $_GET['nombre'];
-    echo "<h1 class='text-center text-white'>Hola " . $nombre . "</h1>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $nombre        = $_POST['nombre'];
+  $apellido      = $_POST['apellido'];
+  $email         = $_POST['email'];
+  $telefono      = $_POST['telefono'];
+  $direccion     = $_POST['direccion'];
+  $fechaNacimiento = $_POST['fechaNacimiento'];
+
+  // Concatenar los valores en una variable
+  $informacionCompleta = "Nombre: " . $nombre . "<br>";
+  $informacionCompleta .= "Apellido: " . $apellido . "<br>";
+  $informacionCompleta .= "Email: " . $email . "<br>";
+  $informacionCompleta .= "Teléfono: " . $telefono . "<br>";
+  $informacionCompleta .= "Dirección: " . $direccion . "<br>";
+  $informacionCompleta .= "Fecha de Nacimiento: " . $fechaNacimiento;
+
+  echo "<h1 class='text-center text-white'>Información completa:</h1>";
+  echo $informacionCompleta;
 }
 ?>
